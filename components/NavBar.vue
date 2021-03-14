2e1c58d0c0a5eaf85627de49fe04dad2594da7a4
Artaghal <wasimkhanak5@gmail.com>
2021-03-14 15:53:48 +0500

    initial work

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,23 +1,16 @@
 <template>
-  <header class="text-gray-600 body-font">
-  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
-      <span class="ml-3 text-xl">Tailblocks</span>
-    </a>
-    <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-      <a class="mr-5 hover:text-gray-900">First Link</a>
-      <a class="mr-5 hover:text-gray-900">Second Link</a>
-      <a class="mr-5 hover:text-gray-900">Third Link</a>
-      <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+  <header class="text-gray-600 body-font bg-blue-800">
+  <div class="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
+    <NuxtLink to="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <img  class="w-10 h-10  p-2 bg-gray-300 rounded-full" src="~/assets/graduation.png">
+      <span class="ml-3 text-xl">Artaghal</span>
+    </NuxtLink>
+    <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4  absolute right-0 	px-20	flex flex-wrap items-end text-gray-300 justify-end">
+      <NuxtLink to='/about' class="mr-5 hover:text-gray-900 hover" >About</NuxtLink>
+      <NuxtLink to='/project' class="mr-5 hover:text-gray-900">Project</NuxtLink>
+      <NuxtLink to='/service' class="mr-5 hover:text-gray-900">Services</NuxtLink>
+      <NuxtLink to='/blog' class="mr-5 hover:text-gray-900">Blog</NuxtLink>
     </nav>
-    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
-    </button>
   </div>
 </header>
 </template>
